1015bec60ca81ae30aa4746556956a91976a4f62
Punnamanikumar <punnamanikumar@gmail.com>
2021-12-27 19:27:11 +0530

    Changes made

--- src/Components/RenderData.js
+++ src/Components/RenderData.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 
 class RenderData extends Component {
+  
   render() {
     return (
       <div className="Childdiv">
-        Name: {this.props.name} | Department: {this.props.department} | Rating:{" "}
-        {this.props.rating}
+        Name: {this.props.name} | Department: {this.props.department} | Rating: {this.props.rating}
       </div>
     );
   }
--- src/Form.js
+++ src/Form.js
@@ -41,49 +41,23 @@ class Form extends Component {
       <div>
         <div className="form">
           <h1>EMPLOYEE FEEDBACK FORM</h1>
+
           <label htmlFor="">Name : </label>
-          <input
-            type="text"
-            name="name"
-            className="input1"
-            onChange={this.getValue}
-            value={this.state.name}
-          />
-          <br />
+          <input type="text" name="name" className="input1" onChange={this.getValue} value={this.state.name}/><br/>
+          
           <label htmlFor="">Department : </label>
-          <input
-            type="text"
-            className="input2"
-            name="department"
-            onChange={this.getValue}
-            value={this.state.department}
-          />
-          <br />
+          <input type="text" className="input2" name="department" onChange={this.getValue} value={this.state.department}/><br/>
+
           <label htmlFor="">Rating : </label>
-          <input
-            className="input3"
-            type="number"
-            name="rating"
-            onChange={this.getValue}
-            value={this.state.rating}
-          />
-          <br />
-          <button name="submit" onClick={this.showValue}>
-            Submit
-          </button>
+          <input className="input3" type="number" name="rating" onChange={this.getValue} value={this.state.rating}/><br />
+
+          <button name="submit" onClick={this.showValue}>Submit</button>
         </div>
 
         <div className="Parentdiv">
-          {this.state.items.map((value, index) => {
-            return (
-              <RenderData
-                name={value.name}
-                department={value.department}
-                rating={value.rating}
-              />
-            );
-          })}
+          {this.state.items.map((value, index) => {return (<RenderData name={value.name} department={value.department} rating={value.rating}/>)})}
         </div>
+        
       </div>
     );
   }
